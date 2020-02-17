98c41a0bae180aaa0ceddf45e0c809cd61963612
Anis Campos <yocorporation@hotmail.com>
2020-02-17 19:58:59 +0100

    Adding Labyrinth parsing

---
+++ labyrinth.py
@@ -0,0 +1,68 @@
+import enum
+
+
+class CellType(enum.Enum):
+    """
+    Enum representing type of cell
+    """
+    WALL = 1
+    HALL = 2
+    START = 3
+    END = 4
+
+
+class Cell:
+
+    def __init__(self, x=0, y=0, cell_type=None):
+        self.x = x
+        self.y = y
+        self.type = cell_type
+
+
+class Labyrinth:
+
+    def __init__(self, nb_row=0, nb_col=0, maze=None, start=None, end=None):
+        """
+
+        :param nb_row:
+        :type nb_row: int
+        :param nb_col: int
+        :param maze: list
+        :param start: Begin position of player
+        :type start: Cell
+        :param end: End game position
+        :type end: Cell
+        """
+        self.nb_row = nb_row
+        self.nb_col = nb_col
+        self.maze = maze
+        self.start = start
+        self.end = end
+
+    @staticmethod
+    def parse(char):
+        """
+        This method is used to parse the string representation of a level
+        :param char:
+        :return:
+        """
+        if char == ' ':
+            return CellType.HALL
+        elif char == '#':
+            return CellType.WALL
+        elif char == 'A':
+            return CellType.START
+        elif char == "B":
+            return CellType.END
+        else:
+            raise Exception("Unknown cell type")
+
+    def load(self, filename):
+        with open(filename) as f:
+            content = f.read().splitlines()
+        self.nb_row = len(content)
+        self.nb_col = len(content[0])
+        self.maze = [Cell(x=i, y=j, cell_type=self.parse(col)) for i, line in enumerate(content) for j, col in
+                     enumerate(line)]
+        self.start = [a for a in self.maze if a.type == CellType.START].pop()
+        self.end = [a for a in self.maze if a.type == CellType.END].pop()
---
+++ labyrinthTest.py
@@ -0,0 +1,18 @@
+import unittest
+from labyrinth import Labyrinth
+
+
+class LabyrinthTestCase(unittest.TestCase):
+    def test_can_load_file(self):
+        labyrinth = Labyrinth()
+        labyrinth.load('./level_1')
+        assert labyrinth.nb_col == 37
+        assert labyrinth.nb_row == 23
+        assert labyrinth.start.x == 22
+        assert labyrinth.start.y == 1
+        assert labyrinth.end.x == 0
+        assert labyrinth.end.y == 35
+
+
+if __name__ == '__main__':
+    unittest.main()
